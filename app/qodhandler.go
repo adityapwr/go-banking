70c8d4da037df9a2bdfba11ad1acd5f2d9421678
Aditya Pawar <pawar.aditya29@gmail.com>
2022-01-06 17:42:09 +0530

    feat: Added QoD backend api

--- app/qodhandler.go
+++ app/qodhandler.go
@@ -1,27 +1,56 @@
 package app
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/adityapwr/go-banking/domain"
 )
 
 type Quote struct {
-	Author string   `json:"author_name"`
-	Quote  string   `json:"quote"`
-	Tags   []string `json:"tags"`
-	Id     string   `json:"id"`
-	Image  string   `json:"image_url"`
-	Length int      `json:"length"`
-}
-
-type content struct {
-	Quotes []Quote `json:"quotes"`
+	Author     string
+	Quote      string
+	Tags       []string
+	Id         string
+	Background string
+	Length     string
+	Category   string
+	Language   string
+	Date       string
+	Permalink  string
+	Title      string
 }
 
 type QoD struct {
-	Sucess  string  `json:"success"`
-	Content content `json:"content"`
+	Success struct {
+		Total int `json:"total"`
+	} `json:"success"`
+	Contents struct {
+		Quotes []struct {
+			Quote  string `json:"quote"`
+			Length string `json:"length"`
+			Author string `json:"author"`
+			Tags   struct {
+				Num0 string `json:"0"`
+				Num1 string `json:"1"`
+				Num2 string `json:"2"`
+				Num3 string `json:"3"`
+				Num5 string `json:"5"`
+			} `json:"tags"`
+			Category   string `json:"category"`
+			Language   string `json:"language"`
+			Date       string `json:"date"`
+			Permalink  string `json:"permalink"`
+			ID         string `json:"id"`
+			Background string `json:"background"`
+			Title      string `json:"title"`
+		} `json:"quotes"`
+	} `json:"contents"`
+	Baseurl   string `json:"baseurl"`
+	Copyright struct {
+		Year int    `json:"year"`
+		URL  string `json:"url"`
+	} `json:"copyright"`
 }
 
 type QoDHandler struct {
@@ -30,6 +59,7 @@ type QoDHandler struct {
 
 func (qh *QoDHandler) GetQoD(w http.ResponseWriter, r *http.Request) {
 	qod, err := qh.repository.GetQod()
+	fmt.Println(qod)
 	if err != nil {
 		writeResponse(w, http.StatusInternalServerError, nil)
 	} else {
--- domain/qod.go
+++ domain/qod.go
@@ -1,6 +1,12 @@
 package domain
 
-import "github.com/adityapwr/banking-lib/errs"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/adityapwr/banking-lib/errs"
+)
 
 type QoDRepository interface {
 	GetQod() (QoD, *errs.AppError)
@@ -9,39 +15,74 @@ type QoDRepository interface {
 type RemoteQoDRepository struct {
 }
 
-type Quote struct {
-	Author string   `json:"author_name"`
-	Quote  string   `json:"quote"`
-	Tags   []string `json:"tags"`
-	Id     string   `json:"id"`
-	Image  string   `json:"image_url"`
-	Length int      `json:"length"`
-}
-
-type content struct {
-	Quotes []Quote `json:"quotes"`
-}
-
 type QoD struct {
-	Sucess  string  `json:"success"`
-	Content content `json:"content"`
+	Success struct {
+		Total int `json:"total"`
+	} `json:"success"`
+	Contents struct {
+		Quotes []struct {
+			Quote  string `json:"quote"`
+			Length string `json:"length"`
+			Author string `json:"author"`
+			Tags   struct {
+				Num0 string `json:"0"`
+				Num1 string `json:"1"`
+				Num2 string `json:"2"`
+				Num3 string `json:"3"`
+				Num5 string `json:"5"`
+			} `json:"tags"`
+			Category   string `json:"category"`
+			Language   string `json:"language"`
+			Date       string `json:"date"`
+			Permalink  string `json:"permalink"`
+			ID         string `json:"id"`
+			Background string `json:"background"`
+			Title      string `json:"title"`
+		} `json:"quotes"`
+	} `json:"contents"`
+	Baseurl   string `json:"baseurl"`
+	Copyright struct {
+		Year int    `json:"year"`
+		URL  string `json:"url"`
+	} `json:"copyright"`
 }
 
 func (r RemoteQoDRepository) GetQod() (QoD, *errs.AppError) {
-	qod := QoD{
-		Sucess: "true",
-		Content: content{
-			Quotes: []Quote{
-				{
-					Author: "Aditya",
-					Quote:  "I am a programmer",
-					Tags:   []string{"programming", "go"},
-					Id:     "1",
-					Image:  "https://cdn.pixabay.com/photo/2016/03/09/09/43/go-lang-1209823_960_720.png",
-					Length: 1,
-				},
-			},
-		},
+	// qod := QoD{
+	// 	Sucess: "true",
+	// 	Content: content{
+	// 		Quotes: []Quote{
+	// 			{
+	// 				Author: "Aditya",
+	// 				Quote:  "I am a programmer",
+	// 				Tags:   []string{"programming", "go"},
+	// 				Id:     "1",
+	// 				Image:  "https://cdn.pixabay.com/photo/2016/03/09/09/43/go-lang-1209823_960_720.png",
+	// 				Length: 1,
+	// 			},
+	// 		},
+	// 	},
+	// }
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", "https://quotes.rest/qod?language=en", nil)
+	if err != nil {
+		log.Fatalln(err)
+		return QoD{}, errs.NewUnexpectedError(err.Error())
+
+	}
+	req.Header.Set("accept", "application/json")
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatalln(err)
+		return QoD{}, errs.NewUnexpectedError(err.Error())
+	}
+	defer resp.Body.Close()
+	log.Println(resp.Body)
+	qod := QoD{}
+
+	if err := json.NewDecoder(resp.Body).Decode(&qod); err != nil {
+		log.Fatalln(err)
+		return QoD{}, errs.NewUnexpectedError(err.Error())
 	}
 	return qod, nil
 
